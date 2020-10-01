27c6ae39d1b730f7a74e1767a951eb0bf0c3334d
Zihe Song <zihe.song@emory.edu>
2020-10-01 12:51:17 -0700

    added files

---
+++ info550-hw2.R
@@ -0,0 +1,8 @@
+hfdata <- read.csv("~/Desktop/2020Fall/INFO550/heart_failure.csv")
+#number of obs
+print(nrow(hfdata))
+#number of missing values
+print(sum(is.na(hfdata)))
+#set where to save the data
+setwd("~/Desktop/2020Fall/INFO550")
+print(getwd())
